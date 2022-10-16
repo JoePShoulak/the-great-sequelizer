6c1c15e1ee157cec9a60e5ce500ceefd58076e3e
Joe P Shoulak <joe@squarecompass.io>
2022-10-16 17:12:41 -0500

    beginning of model definitions

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,11 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: DataTypes.INTEGER,
+    product_name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    stock: DataTypes.INTEGER,
+    category_id: DataTypes.INTEGER
   },
   {
     sequelize,
